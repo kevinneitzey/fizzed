9becee108d1bbddc5d813c8771c806f490884e1c
Kevin <kevin@atozdirectories.com>
2017-10-27 13:51:45 -0400

    Added routing of pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ModePage } from '../pages/mode/mode';
+import { Pack1Page } from '../pages/pack1/pack1';
+import { Pack2Page } from '../pages/pack2/pack2';
+import { Pack3Page } from '../pages/pack3/pack3';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ModePage,
+    Pack1Page,
+    Pack2Page,
+    Pack3Page
   ],
   imports: [
     BrowserModule,
@@ -19,11 +28,16 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ModePage,
+    Pack1Page,
+    Pack2Page,
+    Pack3Page
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ScreenOrientation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import {ModePage} from '../mode/mode';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  modePage = ModePage;
 
   constructor(public navCtrl: NavController) {
 
